05b6ea174df58eb99184e667f1bc90bae379997f
John Ericksen <johncarl81@gmail.com>
2019-09-03 16:07:46 -0600

    Added python experiment running scripts

---
+++ runRhoExperiements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo -e "\nho"
+for rmax in 2 4 8 16
+do
+  echo -e "\nrmax = $rmax"
+  for ((i = 1; i <= $rmax; i++));
+  do
+    echo -e "\nrmin = $i"
+    argos3 -c experiments/coverage/rho/Fault-Tolerance_$i\_$rmax.xml
+  done
+done
